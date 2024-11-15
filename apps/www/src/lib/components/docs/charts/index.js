8b2e9e34414459ff30cc2b47c2cb1f1437cc40e2
Matt O'Brien <666272+mgmobrien@users.noreply.github.com>
2024-11-15 14:05:40 -0700

    Deploy: Fri Nov 15 14:05:40 MST 2024

--- apps/www/src/lib/components/docs/charts/index.js
+++ apps/www/src/lib/components/docs/charts/index.js
@@ -3,3 +3,24 @@ export const Activity = {
   render: () => null,
   $$render: () => ''
 };
+
+export const Metric = {
+  render: () => null,
+  $$render: () => ''
+};
+
+// Add other potential chart components
+export const Bar = {
+  render: () => null,
+  $$render: () => ''
+};
+
+export const Revenue = {
+  render: () => null,
+  $$render: () => ''
+};
+
+export const Subscription = {
+  render: () => null,
+  $$render: () => ''
+};
